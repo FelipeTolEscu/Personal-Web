f6a6d545d2a41d2b8c23935388ca7a686fb2daa4
Felipe <76501827+F3LIP3X@users.noreply.github.com>
2024-05-14 16:31:14 +0200

    1.0.7

--- Ruiz-Lara-web/src/Modulos/tablePanel.tsx
+++ Ruiz-Lara-web/src/Modulos/tablePanel.tsx
@@ -1,42 +1,47 @@
 import '../App.css';
 
+//Import's
+import DesarrolloIng from '../assets/Imagenes/desarrolloIng.jpg' 
+import ProyectosIng from '../assets/Imagenes/proyectosIng.jpg' 
+
 function tablePanel() {
     return (
         <>
             <section className='whitheSection flex justify-center items-center'>
-                <div className="container p-10 py-8 w-auto">
-                    <div className="grid grid-cols-2 gap-6">
-                        <div className="bg-white rounded-lg p-6 h-auto shadow-2xl">
-                            <h2 className="text-xl font-semibold mb-2">Video de nuestras Instalaciones</h2>
+                <div className="container p-4 sm:p-10 py-8 w-full">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                        <div className="bg-white rounded-lg p-4 sm:p-8 h-full shadow-2xl">
+                            <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-gray-800">Video de nuestras Instalaciones</h2>
                             <div className="video-container">
-                                <iframe 
-                                    src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
-                                    title="YouTube video player" 
-                                 
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                <iframe
+                                    src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                                    title="YouTube video player"
+                                    className="w-full h-48 sm:h-72"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 ></iframe>
                             </div>
                         </div>
 
-                        <div className="bg-white rounded-lg p-6 shadow-2xl">
-                            <h2 className="text-xl font-semibold mb-2">El mejor desarrollo</h2>
-                            <img src="ruta/de/la/imagen.jpg" alt="Descripción de la imagen" className="w-full"/>
+                        <div className="bg-white rounded-lg p-4 sm:p-8 h-full shadow-2xl">
+                            <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-gray-800">El mejor desarrollo</h2>
+                            <img src={ProyectosIng} alt="Descripción de la imagen" className="w-full h-48 sm:h-72 object-cover" />
                         </div>
 
-                        <div className="bg-white rounded-lg h-auto p-6 shadow-2xl">
-                            <h2 className="text-xl font-semibold mb-2">Video de nuestros proyectos</h2>
+                        <div className="bg-white rounded-lg p-4 sm:p-8 h-full shadow-2xl">
+                            <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-gray-800">Video de nuestros proyectos</h2>
                             <div className="video-container">
-                                <iframe 
-                                    src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
-                                    title="YouTube video player"  
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                <iframe
+                                    src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                                    title="YouTube video player"
+                                    className="w-full h-48 sm:h-72"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 ></iframe>
                             </div>
                         </div>
 
-                        <div className="bg-white rounded-lg p-6 shadow-2xl">
-                            <h2 className="text-xl font-semibold mb-2">Imagenes Proyectos</h2>
-                            <img src="ruta/de/la/imagen.jpg" alt="Descripción de la imagen" className="w-full"/>
+                        <div className="bg-white rounded-lg p-4 sm:p-8 h-full shadow-2xl">
+                            <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-gray-800">Imagenes Proyectos</h2>
+                            <img src={DesarrolloIng} alt="Descripción de la imagen" className="w-full h-48 sm:h-72 object-cover" />
                         </div>
                     </div>
                 </div>
