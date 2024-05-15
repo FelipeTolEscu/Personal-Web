b54410c37234c30f0ece69cd0bcc05dbed4a3db1
Felipe <76501827+F3LIP3X@users.noreply.github.com>
2024-05-15 20:00:52 +0200

    1.0.8

--- Ruiz-Lara-web/src/Modulos/tablePanel.tsx
+++ Ruiz-Lara-web/src/Modulos/tablePanel.tsx
@@ -1,8 +1,8 @@
-import '../App.css';
+import '../App.css'
 
 //Import's
 import DesarrolloIng from '../assets/Imagenes/desarrolloIng.jpg' 
-import ProyectosIng from '../assets/Imagenes/proyectosIng.jpg' 
+import ProyectosIng from '../assets/Imagenes/proyectosIng.webp' 
 
 function tablePanel() {
     return (
@@ -26,7 +26,10 @@ function tablePanel() {
                             <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-gray-800">El mejor desarrollo</h2>
                             <img src={ProyectosIng} alt="Descripción de la imagen" className="w-full h-48 sm:h-72 object-cover" />
                         </div>
-
+                        <div className="bg-white rounded-lg p-4 sm:p-8 h-full shadow-2xl">
+                            <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-gray-800">Imagenes Proyectos</h2>
+                            <img src={DesarrolloIng} alt="Descripción de la imagen" className="w-full h-48 sm:h-72 object-cover" />
+                        </div>
                         <div className="bg-white rounded-lg p-4 sm:p-8 h-full shadow-2xl">
                             <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-gray-800">Video de nuestros proyectos</h2>
                             <div className="video-container">
@@ -39,10 +42,7 @@ function tablePanel() {
                             </div>
                         </div>
 
-                        <div className="bg-white rounded-lg p-4 sm:p-8 h-full shadow-2xl">
-                            <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-gray-800">Imagenes Proyectos</h2>
-                            <img src={DesarrolloIng} alt="Descripción de la imagen" className="w-full h-48 sm:h-72 object-cover" />
-                        </div>
+                        
                     </div>
                 </div>
             </section>
